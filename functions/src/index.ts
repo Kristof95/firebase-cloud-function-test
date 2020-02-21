605ac20fe707257001303b32676f80a99b2ff9be
Bodnar Kristof <matrixsprt@gmail.com>
2020-02-21 20:00:22 +0100

    test

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,10 +1,9 @@
 import admin from 'firebase-admin';
 import { checkIfEmailExist } from "./user";
 
-const adminConfig = JSON.parse(process.env.FIREBASE_CONFIG as string);
-adminConfig.credential =  admin.credential.cert(require('../service-account-credentials.json'));
-
-admin.initializeApp(adminConfig);
+admin.initializeApp(
+  credential: admin.credential.applicationDefault()
+);
 
 export {
   checkIfEmailExist
