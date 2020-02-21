c3d2cd61b2b7c83b7fcee1b59a97aedbac9d635d
Bodnar Kristof <matrixsprt@gmail.com>
2020-02-21 22:00:50 +0100

    test secret

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,18 @@
 import admin from 'firebase-admin';
 import { checkIfEmailExist } from "./user";
 
-admin.initializeApp(
-  {credential: admin.credential.applicationDefault()},
-);
+// admin.initializeApp(
+//   {credential: admin.credential.applicationDefault()},
+// );
+
+let secret = process.env.DEFAULT_SERVICE_ACCOUNT_KEY
+const serviceAccount = require(secret)
+
+const adminConfig = JSON.parse(process.env.FIREBASE_CONFIG as string);
+adminConfig.credential = admin.credential.cert(serviceAccount);
+adminConfig.databaseURL = functions.config().config.database;
+
+admin.initializeApp(adminConfig);
 
 export {
   checkIfEmailExist
